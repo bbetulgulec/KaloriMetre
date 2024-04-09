93db7e7e861245f13dc102d72f2fa088f759228d
yasinyyilmaz <yasinyilmazofficial@hotmail.com>
2024-04-09 18:16:14 +0300

    Sadece kayıt sayfası yapıldı.

--- app/src/main/java/com/betulgulec/app/kayitol.java
+++ app/src/main/java/com/betulgulec/app/kayitol.java
@@ -2,65 +2,96 @@ package com.betulgulec.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class kayitol extends AppCompatActivity {
 
-    private TextInputLayout adLayout, soyadLayout, mailLayout, telefonLayout, passwordLayout;
-    private Button btnkayitOl;
+    private EditText editTextad, editTextsoyad, editTextmail, editTexttelefon, editTextpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayitol);
-        initComponents();
-    }
 
-    private void initComponents() {
-        adLayout = findViewById(R.id.adLayout);
-        soyadLayout = findViewById(R.id.soyadLayout);
-        mailLayout = findViewById(R.id.mailLayout);
-        telefonLayout = findViewById(R.id.telefonLayout);
-        passwordLayout = findViewById(R.id.passwordLayout);
-        btnkayitOl = findViewById(R.id.btnkayitOl);
-        btnkayitOl.setOnClickListener(new View.OnClickListener() {
+        editTextad = findViewById(R.id.ad);
+        editTextsoyad = findViewById(R.id.soyad);
+        editTextmail = findViewById(R.id.mail);
+        editTexttelefon = findViewById(R.id.telefon);
+        editTextpassword = findViewById(R.id.password);
+        Button kayitbutonu = findViewById(R.id.btnkayitOl);
+        kayitbutonu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (validateEditText(adLayout) && validateEditText(soyadLayout) &&
-                        validateEditText(mailLayout) && validateEditText(telefonLayout) &&
-                        validateEditText(passwordLayout)) {
-                    startActivity(new Intent(kayitol.this, girisyap.class));
-                    finish(); // Bu aktiviteyi kapat
+                String textAd = editTextad.getText().toString();
+                String textSoyad = editTextsoyad.getText().toString();
+                String textMail = editTextmail.getText().toString();
+                String textTelefon = editTexttelefon.getText().toString();
+                String textPassword = editTextpassword.getText().toString();
+
+                if (TextUtils.isEmpty(textAd)) {
+                    editTextad.setError("Lütfen adınızı giriniz.");
+                    editTextad.requestFocus();
+                } else if (TextUtils.isEmpty(textSoyad)) {
+                    editTextsoyad.setError("Lütfen soyadınızı giriniz.");
+                    editTextsoyad.requestFocus();
+                } else if (TextUtils.isEmpty(textMail) || !isValidEmail(textMail)) {
+                    editTextmail.setError("Lütfen geçerli bir e-posta adresi giriniz.\nÖrnek: example@example.com");
+                    editTextmail.requestFocus();
+                } else if (TextUtils.isEmpty(textTelefon)) {
+                    editTexttelefon.setError("Lütfen telefon numaranızı giriniz.");
+                    editTexttelefon.requestFocus();
+                } else if (TextUtils.isEmpty(textPassword) || textPassword.length() < 6) {
+                    editTextpassword.setError("Şifreniz en az 6 karakter uzunluğunda olmalıdır.");
+                    editTextpassword.requestFocus();
                 } else {
-                    if (!validateEditText(adLayout)) {
-                        adLayout.setError("Lütfen Adınızı giriniz");
-                    }
-                    if (!validateEditText(soyadLayout)) {
-                        soyadLayout.setError("Lütfen Soyadınızı giriniz");
-                    }
-                    if (!validateEditText(mailLayout)) {
-                        mailLayout.setError("Lütfen Mail adresinizi giriniz");
-                    }
-                    if (!validateEditText(telefonLayout)) {
-                        telefonLayout.setError("Lütfen Telefon numaranızı giriniz");
-                    }
-                    if (!validateEditText(passwordLayout)) {
-                        passwordLayout.setError("Lütfen Şifre oluşturunuz ");
-                    }
-                    Toast.makeText(kayitol.this, "Lütfen tüm alanları doldurun.", Toast.LENGTH_SHORT).show();
+                    registerUser(textAd, textSoyad, textMail, textTelefon, textPassword);
                 }
             }
         });
     }
 
-    private boolean validateEditText(TextInputLayout layout) {
-        String input = layout.getEditText().getText().toString().trim();
-        return !input.isEmpty();
+    private void registerUser(String textAd, String textSoyad, String textMail, String textTelefon, String textPassword) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(textMail, textPassword).addOnCompleteListener(kayitol.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(kayitol.this, "Kullanıcı kaydı başarılı.", Toast.LENGTH_LONG).show();
+
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            firebaseUser.sendEmailVerification();
+
+                            // Girisyap ekranına geçiş yap
+                            startActivity(new Intent(kayitol.this, girisyap.class));
+                            finish(); // Kayitol aktivitesini kapat
+                        } else {
+                            Toast.makeText(kayitol.this, "Kullanıcı kaydı başarısız! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public void kayitol(View view) {
+        Intent intent = new Intent(this, girisyap.class);
+        startActivity(intent);
     }
 }
